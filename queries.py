01f77f1d01fd721586f7beb2c69ff70af383863f
Peter <pesomerville@peter-somervilles-macbook-pro.local>
2018-04-12 14:44:47 +0800

    looking for cities with exchanges

--- queries.py
+++ queries.py
@@ -3,12 +3,22 @@ from pprint import pprint
 
 ckds = CKDS()
 
+# City for each exchange
+
+exchanges_list = ckds.db.open().query(Exchange).all()
+for exchange in exchanges_list:
+    exchange_cities = exchange.city
+    results = ckds.get_json("http://data.coding.kitchen/api/city/{}".format(exchange_cities))
+    print(results)
+    print(results.name)
+
+
+
+
+# high_rev_cos = ckds.db.open().query(Company).filter(Company.revenue > 1000).all()
+# for high_rev_co in high_rev_cos:
+#     pprint(high_rev_co.name)
+
 # state_capitals = ckds.db.open().query(City).filter(City.is_capital == 1).all()
 # for state_capital in state_capitals:
 #     pprint(state_capital.name)
-
-# exchange_cities = ckds.db.open().query(Exchange).filter(Exchange.city == )
-
-high_rev_cos = ckds.db.open().query(Company).filter(Company.revenue > 1000).all()
-for high_rev_co in high_rev_cos:
-    pprint(high_rev_co.name)
